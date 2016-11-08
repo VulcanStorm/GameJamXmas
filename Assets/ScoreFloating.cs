ef4f44f14a913577de9f1587348084f2cb5d7cf0
James <vulcanstorm98@gmail.com>
2016-11-08 14:15:44 +0000

    basic stuff

---
+++ Assets/ScoreFloating.cs
@@ -0,0 +1,95 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+
+public class ScoreFloating : MonoBehaviour
+{
+	RectTransform thisRectTransform;
+	Text displayText;
+	public bool isShown = false;
+	float timer = 0;
+	float wobbleTimer = 0;
+	bool doneFloat;
+
+	static Quaternion leftRot = Quaternion.Euler (0, 0, 10);
+	static Quaternion rightRot = Quaternion.Euler (0, 0, -10);
+	Quaternion fromRot;
+	Quaternion toRot;
+
+	static float floatTime = 1.75f;
+	static float wobbleTime = 0.25f;
+	static float stopWobbleTime = 0.2f;
+	static float zoomTime = 2.75f;
+
+	public static Vector2 player1ScoreRect;
+	public static Vector2 player2ScoreRect;
+	public static Vector2 player3ScoreRect;
+	public static Vector3 player4ScoreRect;
+	Vector2 targetScoreRect;
+	Vector2 floatPos;
+
+	void Awake ()
+	{
+		thisRectTransform = GetComponent<RectTransform> ();
+		thisRectTransform.rotation = leftRot;
+		fromRot = leftRot;
+		toRot = rightRot;
+	}
+
+	void Display (int scoreValue, int playerCorner)
+	{
+		displayText.text = scoreValue.ToString ();
+		timer = 0;
+		wobbleTimer = 0;
+		thisRectTransform.rotation = fromRot;
+		switch (playerCorner) {
+		case 1:
+			targetScoreRect = player1ScoreRect;
+			break;
+		case 2:
+			targetScoreRect = player2ScoreRect;
+			break;
+		case 3:
+			targetScoreRect = player3ScoreRect;
+			break;
+		case 4:
+			targetScoreRect = player4ScoreRect;
+			break;
+		default:
+			throw new UnassignedReferenceException ("This player corner does not exist " + playerCorner);
+		}
+
+	}
+
+	void Update ()
+	{
+		if (isShown == true) {
+			
+			timer += Time.deltaTime;
+			if (timer < floatTime) {
+				wobbleTimer += Time.deltaTime;
+				if (wobbleTimer > wobbleTime) {
+					Quaternion temp = fromRot;
+					fromRot = toRot;
+					toRot = temp;
+					wobbleTimer = 0;
+				}
+				thisRectTransform.rotation = Quaternion.Lerp (fromRot, toRot, (wobbleTimer / wobbleTime));
+			} else {
+				if (doneFloat = false) {
+					doneFloat = true;
+					timer = 0;
+				}
+					
+				if (timer < zoomTime) {
+					thisRectTransform.rotation = Quaternion.Lerp (thisRectTransform.rotation, Quaternion.identity, timer / zoomTime);
+					thisRectTransform.position = Vector3.Lerp (floatPos, targetScoreRect, timer * timer / zoomTime);
+				} else {
+					// hide this score
+				}
+			}
+
+		}
+	}
+}
---
+++ Assets/XBoxCtrlInputs.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class XBoxCtrlInputs
+{
+	public int controllerNumber;
+
+	public bool aButton;
+	public bool bButton;
+	public bool xButton;
+	public bool yButton;
+
+	public bool startButton;
+	public bool selectButton;
+
+	public float leftStickX;
+	public float leftStickY;
+	public bool leftStickPressed;
+	public float rightStickX;
+	public float rightStickY;
+	public bool rightStickPressed;
+
+	public float leftTrigger;
+	public float rightTrigger;
+	public bool leftBumper;
+	public bool rightBumper;
+
+
+
+	// Use this for initialization
+	void Start ()
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void UpdateController ()
+	{
+		// update our input
+		switch (controllerNumber) {
+		case 1:
+
+			break;
+		case 2:
+
+			break;
+		case 3:
+
+			break;
+
+		case 4:
+
+			break;
+
+		default:
+			throw new MissingReferenceException ("This controller index doesn't exist: " + controllerNumber.ToString ());
+		}
+	}
+
+	private void GetPlayer1Input ()
+	{
+		// TODO fill this in, and make it work
+		// get each button based off the joystick directly.
+	}
+	// TODO make functions for the other 3 players.
+}
+
+
